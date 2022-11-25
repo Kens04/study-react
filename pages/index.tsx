db0ade9b6378060c1ca37bb2f8685decc59820eb
John Doe <johndoe@example.com>
2022-11-25 22:31:54 +0900

    ♻️ ヘッドライン部分をコンポーネント化

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Links from "../components/Links";
+import Headline from "../components/Headline";
 import { Footer } from "../components/Footer";
 
 export default function Home() {
@@ -14,13 +15,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Index Page</h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
+        <Headline page="index" />
         <Links />
       </main>
 

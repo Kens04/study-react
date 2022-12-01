0b908e8453567002671ae52cafe9f090206400cc
John Doe <johndoe@example.com>
2022-12-01 09:07:03 +0900

    ✨ ユーザーのfetch、fetcherの共通化

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -3,18 +3,14 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from "src/components/Footer";
 import { Main } from 'src/components/Main';
 import { Header } from "src/components/Header";
-import { useRouter } from 'next/router';
+import { Post } from 'src/components/Post';
 
 const PostId = () => {
-  const router = useRouter();
 
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Index Page</title>
-      </Head>
       <Header />
-      <div>{router.query.id}</div>
+      <Post />
     </div>
   )
 }

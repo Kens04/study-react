72acef1e8032eb572ce6f3c0a986a1f04b8d830f
John Doe <johndoe@example.com>
2022-12-03 14:17:25 +0900

    ✨ ユーザーページに投稿一覧を追加

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import { useRouter } from 'next/router';
 import { Header } from "src/components/Header";
-import { Post } from 'src/components/Post';
-import { useUser } from 'src/hooks/useUser';
 import { UserComponent } from 'src/components/User';
 
 const UsersId = () => {

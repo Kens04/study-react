f4d259c52654d6b68cf9dfaadaef3b58bf4ec925
John Doe <johndoe@example.com>
2022-12-03 15:00:56 +0900

    ✨ コメントページで元の投稿を表示

---
+++ src/components/Post/PostByCommentId.jsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Link from "next/link";
+import { usePost } from "src/hooks/usePost";
+
+export const PostByCommentId = (props) => {
+  const { data, error, isLoading } = usePost(props.id);
+
+  if(isLoading) {
+    return <div>ローディング中</div>;
+  }
+
+  if(error) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+  <Link href={`/posts/${data?.id}`}>
+    {data?.title}
+  </Link>
+  );
+};
+
+export default PostByCommentId;
